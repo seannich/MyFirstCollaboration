085d707cb585bfc386218f4023d67796064c4276
Sean Nicholas <seannts@outlook.com>
2020-11-11 14:39:04 +0800

    merged all

--- hello_world.py
+++ hello_world.py
@@ -1,18 +1,13 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 print("hello world!")
 
 print("This is hello from Gerald. I am adding a new line of code here.")
 
 print("Hello again. I am here to add in a second line. I am Gerald")
-=======
+
 print("This is hello from Naomi. I am adding a new line of code here.")
 
 
 
 
 print("Hello again. I am here to add in a second line. I am Naomi.")  
->>>>>>> track_Naomi
-=======
-print("hello world!")
->>>>>>> track_Adel
+
